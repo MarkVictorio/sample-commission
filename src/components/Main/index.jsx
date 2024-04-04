3c27f418552f587a18e0ce3c49b0c3d62e00f47e
Mark Victorio <mark.victorio@hoolisoftware.com>
2024-04-05 00:03:03 +0800

    first commit

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,13 @@
+const Main = () => {
+  return (
+    <div className="m-auto p-4">
+        <div className="text-xl font-thin">
+            Dashboard
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+
+  
+    return (
+      <div className='bg-[#ffffff] flex justify-between items-center h-14 mx-auto p-4 text-black shadow-xl'>
+        {/* Logo */}
+        <h1 className='w-full text-xl font-bold text-[#9373ec]'> Logo Here </h1>
+  
+      </div>
+    );
+  };
+  
+  export default Navbar;
